f1aae3ead01410e024908f74f6e39a2f374118ef
pasha <pasha2001dpa@ya.ru>
2022-06-28 18:24:19 +0300

    Initial commit

---
+++ pydatastream/__init__.py
@@ -0,0 +1,6 @@
+from .utils import float_to_bytes, float_from_bytes, normalize_byteorder
+from .stream_wrapper import StreamWrapper, SEEK_CUR, SEEK_SET, SEEK_END
+from .data_input_stream import DataInputStream
+from .data_output_stream import DataOutputStream
+
+__version__ = "0.1.0"
